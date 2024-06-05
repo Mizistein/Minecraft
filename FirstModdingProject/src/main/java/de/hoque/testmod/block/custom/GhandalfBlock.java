002c4e7e3670e496658f01165f5bcb853f8315f6
Mizi <Mizi@DESKTOP-98S99BV>
2024-06-05 18:39:50 +0200

    Ghandalf der scharfe update

--- FirstModdingProject/src/main/java/de/hoque/testmod/block/custom/GhandalfBlock.java
+++ FirstModdingProject/src/main/java/de/hoque/testmod/block/custom/GhandalfBlock.java
@@ -24,6 +24,7 @@ public class GhandalfBlock extends Block
    @Override
    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity)
    {
+	  
       pEntity.sendSystemMessage(Component.literal("GET OFF MEEE ALLLAAAAAAAAAAAAHUAKBAAAAAAAAAAAAAAAR"));
       
       super.stepOn(pLevel, pPos, pState, pEntity);
@@ -34,11 +35,22 @@ public class GhandalfBlock extends Block
     		BlockHitResult pHit)
    {
 	  pLevel.playSound(pPlayer, pPos, SoundEvents.TNT_PRIMED, SoundSource.BLOCKS, 1.0F, 1.0F);
-	  if(Math.random() * 100 < 90.0)
+	  
+	  if(!pLevel.isClientSide())
 	  {
-		 pPlayer.sendSystemMessage(Component.literal("Allahuakbar"));
-		 pPlayer.kill();
+		 if((Math.random() * 100.0) < 10.0)
+		  {
+			 pPlayer.sendSystemMessage(Component.literal("Allahuakbar"));
+			 pLevel.explode(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), 5.0F, true, Level.ExplosionInteraction.TNT);
+			 pPlayer.kill();
+			 if(pPlayer.isDeadOrDying())
+			 {
+				pLevel.playSound(pPlayer, pPos, SoundEvents.GENERIC_EXPLODE, SoundSource.BLOCKS, 0.0F, 0.0F);
+				pPlayer.sendSystemMessage(Component.literal("BOOM"));
+			 }
+		  }
 	  }
+	  
 	  
       return InteractionResult.SUCCESS;
    }
